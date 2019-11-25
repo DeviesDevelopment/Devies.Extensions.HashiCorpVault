4b38f806e395b708ebd50bddd57d6b032b356e49
John Johansson <johanssonjohn91@gmail.com>
2019-11-25 13:44:27 +0100

    Better login

--- VaultUtils.cs
+++ VaultUtils.cs
@@ -8,50 +8,67 @@ namespace Devies.Extensions.HashiCorpVault
 {
     public static class VaultUtils
     {
-        public static string GetVaultToken(string url, string role, string user, string pass)
+        public static string LoginWithUserpass(string url, string user, string pass)
         {
             IRestClient client = new RestClient($"{url}/v1");
-            string clientToken = null;
-
-            if (File.Exists("/run/secrets/kubernetes.io/serviceaccount/token"))
+            
+            if (string.IsNullOrWhiteSpace(user))
             {
-                var token = File.ReadAllLines("/run/secrets/kubernetes.io/serviceaccount/token")[0];
+                return null;
+            }
 
-                IRestRequest request = new RestRequest("auth/kubernetes/login", Method.POST);
-                var requestBody = JsonConvert.SerializeObject(new {jwt = token, role = role});
+            if (string.IsNullOrWhiteSpace(pass))
+            {
+                return null;
+            }
+            
+            IRestRequest request = new RestRequest($"auth/userpass/login/{user}", Method.POST);
+            var requestBody = JsonConvert.SerializeObject(new {password = pass});
 
-                request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
-                var response = client.Execute(request);
+            request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
+            var response = client.Execute(request);
 
-                if (response.IsSuccessful)
+            if (response.IsSuccessful)
+            {
+                var data = JsonConvert.DeserializeObject<VaultToken>(response.Content);
+                if (data?.Auth?.ClientToken?.Length > 1)
                 {
-                    var data = JsonConvert.DeserializeObject<VaultToken>(response.Content);
-                    if (data?.Auth?.ClientToken?.Length > 1)
-                    {
-                        clientToken = data.Auth.ClientToken;
-                    }
+                    return data.Auth.ClientToken;
                 }
             }
 
-            if (clientToken == null && !string.IsNullOrWhiteSpace(user) && !string.IsNullOrWhiteSpace(pass))
+            return null;
+        }
+
+
+        public static string LoginWithK8SServiceAccount(string url, string role)
+        {
+            IRestClient client = new RestClient($"{url}/v1");
+
+            if (!File.Exists("/run/secrets/kubernetes.io/serviceaccount/token"))
             {
-                IRestRequest request = new RestRequest($"auth/userpass/login/{user}", Method.POST);
-                var requestBody = JsonConvert.SerializeObject(new {password = pass});
+                return null;
+            }
 
-                request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
-                var response = client.Execute(request);
+            var token = File.ReadAllLines("/run/secrets/kubernetes.io/serviceaccount/token")[0];
 
-                if (response.IsSuccessful)
+            IRestRequest request = new RestRequest("auth/kubernetes/login", Method.POST);
+            var requestBody = JsonConvert.SerializeObject(new {jwt = token, role = role});
+
+            request.AddParameter("application/json", requestBody, ParameterType.RequestBody);
+            var response = client.Execute(request);
+
+            if (response.IsSuccessful)
+            {
+                var data = JsonConvert.DeserializeObject<VaultToken>(response.Content);
+                if (data?.Auth?.ClientToken?.Length > 1)
                 {
-                    var data = JsonConvert.DeserializeObject<VaultToken>(response.Content);
-                    if (data?.Auth?.ClientToken?.Length > 1)
-                    {
-                        clientToken = data.Auth.ClientToken;
-                    }
+                    return data.Auth.ClientToken;
                 }
             }
 
-            return clientToken;
+
+            return null;
         }
     }
 }
